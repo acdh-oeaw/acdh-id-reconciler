3a80390a4dabcff3362318b3b61fa1ddb013804d
csae8092 <peter.andorfer@oeaw.ac.at>
2022-10-14 13:29:51 +0200

    gnd_to_wikidata #1

--- tests/test_reconciler.py
+++ tests/test_reconciler.py
@@ -1,5 +1,5 @@
 import unittest
-from acdh_id_reconciler import gnd_to_geonames
+from acdh_id_reconciler import gnd_to_geonames, gnd_to_wikidata
 
 DATA = [
     (
@@ -18,7 +18,23 @@ DATA = [
             'geonames': '2781124'
         }
     )
+]
 
+DATA_GND_TO_WIKIDATA = [
+    (
+        "http://d-nb.info/gnd/4074255-6/",
+        {
+            'wikidata': 'http://www.wikidata.org/entity/Q41329',
+            'gnd': '4074255-6',
+        }
+    ),
+    (
+        "https://d-nb.info/gnd/4010858-2",
+        {
+            'wikidata': 'http://www.wikidata.org/entity/Q261664',
+            'gnd': '4010858-2',
+        }
+    )
 ]
 
 
@@ -34,4 +50,9 @@ class TestTestTest(unittest.TestCase):
     def test_001_gnd_to_geonames(self):
         for x in DATA:
             result = gnd_to_geonames(x[0])
+            self.assertEqual(result, x[1])
+
+    def test_002_gnd_to_wikidata(self):
+        for x in DATA_GND_TO_WIKIDATA:
+            result = gnd_to_wikidata(x[0])
             self.assertEqual(result, x[1])
