68d7506a438c99cc1622638efbb012c94c45a7d5
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-11-14 13:57:04 +0100

    added id reconciler for geoneames to gnd and wikidata

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/acdh-oeaw/acdh-id-reconciler',
-    version="0.3",
+    version="0.4",
     zip_safe=False,
 )
